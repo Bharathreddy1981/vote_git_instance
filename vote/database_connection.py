71b85528f0c149defc51121c04a14d94bad6f3c2
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-12-04 19:21:19 +0530

    changing the database to rds database

--- vote/database_connection.py
+++ vote/database_connection.py
@@ -2,10 +2,10 @@ import pymysql
 def data():
 
     connect = pymysql.connect(
-                host = "localhost",
-                user = "root",
-                passwd = "Vangala@09",
-                db = "votepoll"
+        host="database-1.cfmudpl0e2d7.ap-south-1.rds.amazonaws.com",
+        user="admin",
+        passwd="bharath**",
+        db="transction_prod"
     )
 
     return connect
