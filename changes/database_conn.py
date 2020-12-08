a6d1b4fa67bc187024fd62ea07ad781e4c8899eb
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-12-08 17:59:57 +0530

    ch

--- changes/database_conn.py
+++ changes/database_conn.py
@@ -1,11 +1,11 @@
 import pymysql
 def data():
-
+    
     connect = pymysql.connect(
-                host = "localhost",
-                user = "root",
-                passwd = "Vangala@09",
-                db = "votepoll"
+        host="database-1.cfmudpl0e2d7.ap-south-1.rds.amazonaws.com",
+        user="admin",
+        passwd="bharath**",
+        db="transction_prod"
     )
 
     return connect
